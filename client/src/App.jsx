71b237341f05387ab42fd76e9ba51b88d057281a
Sanjay Pranya <sanjay.pranya11@gmail.com>
2026-06-21 16:22:11 +0530

    Added user registration UI and user listing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,101 @@
-import { useEffect, useState } from "react";
+
+import { useState, useEffect } from "react";
 import API from "./api/api";
 
-function App() {
-  const [message, setMessage] = useState("");
+function App(){
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-  useEffect(() => {
-    API.get("/")
-      .then((response) => {
-        setMessage(response.data); 
-        // Stores the msg ^ from index.js in above
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    fetchUsers();
   }, []);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const fetchUsers = async () => {
+    try {
+      const res = await API.get("/api/users");
+      setUsers(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await API.post(
+        "/api/users/register",
+        formData
+      );
+
+      alert(res.data.message);
+      fetchUsers();
+
+      setFormData({
+        name: "",
+        email: "",
+        password: "",
+      });
+    } catch (error){
+      console.log(error);
+    }
+  };
 
   return (
     <div>
       <h1>StudentOS</h1>
-      <h2>{message}</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Name"
+          onChange={handleChange}
+          value={formData.name}
+        />
+        <br/><br/>
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          onChange={handleChange}
+          value={formData.email}
+        />
+        <br/><br/>
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          onChange={handleChange}
+          value={formData.password}
+        />
+        <br/><br/>
+
+        <button type="submit">
+          Register
+        </button>
+      </form>
+      <h2>All Users</h2>
+
+      {users.map((user)=> (
+        <div key={user.id}>
+          <p>
+            {user.name} - {user.email}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
