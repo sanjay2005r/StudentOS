436cb7f6d99d00ae2ebe3eaa80d8b6ab28f04d0f
Sanjay Pranya <sanjay.pranya11@gmail.com>
2026-07-23 23:34:19 +0530

    Added the Placement updated form

--- client/src/pages/Placement.jsx
+++ client/src/pages/Placement.jsx
@@ -1,4 +1,8 @@
-import { useState } from "react";
+import API from "../api/api";
+import { useState, useEffect } from "react";
+
+
+
 function Placement() {
     const [placementData, setPlacementData] = useState({
         company: "",
@@ -7,22 +11,83 @@ function Placement() {
         deadline: "",
         round_name: "",
     });
+
+    const [placements, setPlacements] = useState([]);
+
+    const handleChange = (e) => {
+        setPlacementData({
+            ...placementData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const addPlacement = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await API.post(
+                "/api/placements",
+                placementData
+            );
+            alert(res.data.message);
+            fetchPlacements();
+            setPlacementData({
+                company: "",
+                role: "",
+                status: "Applied",
+                deadline: "",
+                round_name: "",
+            });
+        }
+
+        catch(error){
+            console.log(error);
+        }
+    };
+
+    const fetchPlacements = async () => {
+        try {
+            const res = await API.get(
+                "/api/placements"
+            );
+            setPlacements(res.data);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchPlacements();
+    }, []);
+
     return(
-        <div>
+        <form onSubmit={addPlacement}>
+        
             <h2>💼 Placement Tracker</h2>
             <input
                 type="text"
+                name="company"
+                value={placementData.company}
+                onChange={handleChange}
                 placeholder="Company"
             />
             <br/><br/>
 
             <input
                 type="text"
+                name="role"
+                value={placementData.role}
+                onChange={handleChange}
                 placeholder="Role"
             />
             <br/><br/>
 
-            <select>
+            <select
+                name="status"
+                value={placementData.status}
+                onChange={handleChange}
+            >
                 <option>Applied</option>
                 <option>OA</option>
                 <option>Interview</option>
@@ -33,17 +98,25 @@ function Placement() {
 
             <input
                 type="date"
+                name="deadline"
+                value={placementData.deadline}
+                onChange={handleChange}
             />
             <br/><br/>
+
             <input
                 type="text"
+                name="round_name"
+                value={placementData.round_name}
+                onChange={handleChange}
                 placeholder="Current Round"
             />
             <br/><br/>
-            <button>
+
+            <button type="submit">
                 Add Placement
             </button>
-        </div>
+        </form>
     );
 }
 
